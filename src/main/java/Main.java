3437d130a7ca0cc2b818d5baf34df2dee406c948
hyunsoojung <jhs695@naver.com>
2019-10-04 22:07:55 +0900

    feat: Main 생성

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import model.Car;
+import model.Cars;
+import utils.RandomGenerator;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> carNames = InputView.inputCarNames();
+        Cars cars = new Cars(carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList()));
+
+        int count = InputView.inputCount();
+
+        for (int i = 0; i < count; i++) {
+            cars = cars.moveCars(RandomGenerator.generateRandomNumber(cars.getSize()));
+            OutputView.printRoundResult(cars);
+        }
+
+        OutputView.printWinners(cars.getWinners());
+    }
+}
