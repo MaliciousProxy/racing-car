e31d706c90b91880db11f147e66d9260e8a6fea5
hyunsoojung <jhs695@naver.com>
2019-10-04 21:45:33 +0900

    feat: move(), isSamePosition(), getPosition() 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,12 +1,48 @@
 package model;
 
+import java.util.Objects;
+
 public class Car {
     private final String carName;
+    private int position;
 
     public Car(final String carName) {
+        this(carName, 0);
+    }
+
+    private Car(String carName, int position) {
         if (carName.length() > 5 || carName.length() <= 0) {
             throw new IllegalArgumentException();
         }
         this.carName = carName;
+        this.position = position;
+    }
+
+    public Car move(int number) {
+        if (number >= 4) {
+            return new Car(this.carName, ++this.position);
+        }
+        return this;
+    }
+
+    public boolean isSamePosition(int position) {
+        return this.position == position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return carName.equals(car.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName);
     }
 }
--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -2,6 +2,7 @@ package model;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
@@ -19,5 +20,15 @@ class CarTest {
     @Test
     void 차_생성_이름_0자_예외() {
         assertThrows(IllegalArgumentException.class, () -> new Car(""));
+    }
+
+    @Test
+    void 차_움직임_3일_때_테스트() {
+        assertThat(new Car("a").move(3).getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void 차_움직임_4일_때_테스트() {
+        assertThat(new Car("a").move(4).getPosition()).isEqualTo(1);
     }
 }
