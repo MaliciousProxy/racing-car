2c847b519edd0b75616d829c3265b72b1c6cbc25
hyunsoojung <jhs695@naver.com>
2019-10-04 22:07:44 +0900

    feat: OutputView 생성

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -33,6 +33,10 @@ public class Car {
         return position;
     }
 
+    public String getName() {
+        return carName;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
