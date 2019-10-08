067af9da9b2eea1365dce5c1fd48323d8299a1f5
hyunsoojung <jhs695@naver.com>
2019-10-09 02:03:48 +0900

    chore: temp commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         List<String> carNames = InputView.inputCarNames();
+
         Cars cars = new Cars(carNames.stream()
                 .map(CarName::new)
                 .map(Car::new)
